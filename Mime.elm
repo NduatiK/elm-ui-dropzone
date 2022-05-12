036ee921842eb9098c96ff58e1ad49e8b24cb681
Kuria <012nkuria@gmail.com>
2022-05-12 09:49:29 +0200

    Create Mime.elm

---
+++ Mime.elm
@@ -0,0 +1,8 @@
+module Mime exposing (images)
+
+-- This can be replaced with a full library eg
+-- https://package.elm-lang.org/packages/danyx23/elm-mimetype/latest/MimeType
+
+images : List String
+images =
+    [ "image/png", "image/jpg", "image/jpeg" ]
